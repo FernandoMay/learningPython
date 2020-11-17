98f19f0dc1ea84601ef1f2b15f9565cc6df1f9e7
Fernando May <fmay130@gmail.com>
2020-11-17 11:58:25 -0600

    bubbles

---
+++ burbujas.py
@@ -0,0 +1,13 @@
+lista1 = [4,8,15,16,23,42]
+
+def burbuja(lista):
+    for i in range(len(lista)-1,0,-1):
+        for j in range(dato): 
+            if lista[i]>lista[i+1]:
+                temp = lista[i]
+                lista[i] = lista[i+1]
+                lista[i+1] = temp
+
+burbuja(lista1)
+
+print(lista1)
